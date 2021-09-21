316c5b2d236fb1592611e5df5b4f4ed5a653d534
deny fitra <66256099+dfitraa@users.noreply.github.com>
2021-09-22 02:25:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Tabungan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabungan tabungan = new Tabungan(0);
+        Menu:
+            Console.Clear();
+            Console.WriteLine("Pilih fitur yang anda ingin gunakan");
+            Console.WriteLine("1. Menabung");
+            Console.WriteLine("2. Cek Saldo");
+            Console.WriteLine("3. Ambil Uang");
+            Console.WriteLine("4. Ambil Uang (Dollar)");
+            Console.WriteLine("5. Keluar");
+            Console.Write("Pilih: ");
+
+            int pilihMenu = Convert.ToInt32(Console.ReadLine());
+            switch (pilihMenu)
+            {
+                case 1:
+                    Console.Write("Masukkan uang (Biaya Admin Rp.5000) : Rp ");
+                    int tabung = Convert.ToInt32(Console.ReadLine());
+                    bool hasil = tabungan.simpanUang(tabung);
+                    if (hasil)
+                    {
+                        Console.WriteLine("Berhasil");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gagal, uang yang ditabung harus lebih dari Rp.5000");
+                    }
+                    Console.ReadLine();
+                    goto Menu;
+
+                case 2:
+                    Console.Write("Saldo Anda: Rp ");
+                    Console.Write(tabungan.cekSaldo());
+                    Console.ReadLine();
+                    goto Menu;
+
+                case 3:
+                    Console.Write("Masukkan uang yang diambil: Rp ");
+                    int ambilRupiah = Convert.ToInt32(Console.ReadLine());
+                    bool hasilRupiah = tabungan.ambilRupiah(ambilRupiah);
+                    if (hasilRupiah)
+                    {
+                        Console.WriteLine("Berhasil");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gagal, uang yang diambil kurang");
+                    }
+                    Console.ReadLine();
+                    goto Menu;
+
+                case 4:
+                    Console.Write("Masukkan uang yang ingin diambil (Dollar) : $ ");
+                    int ambilDollar = Convert.ToInt32(Console.ReadLine());
+                    bool hasilDollar = tabungan.ambilDollar(ambilDollar);
+                    if (hasilDollar)
+                    {
+                        Console.WriteLine("Berhasil");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gagal, uang yang diambil kurang");
+                    }
+                    Console.ReadLine();
+                    goto Menu;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Tabungan.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Tabungan
+{
+    class Tabungan
+    {
+        private int saldo;
+
+        public Tabungan(int initial_saldo)
+        {
+            saldo = initial_saldo;
+        }
+
+        public int cekSaldo()
+        {
+            return saldo;
+        }
+
+        public bool simpanUang(int jumlah)
+        {
+            if(biayaAdministrasi(jumlah) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                jumlah = biayaAdministrasi(jumlah);
+                saldo += jumlah;
+                return true;
+            }
+        }
+
+        public int biayaAdministrasi(int jumlah)
+        {
+            jumlah -= 5000;
+            return jumlah;
+        }
+
+        public bool ambilRupiah(int jumlah)
+        {
+            if ((saldo - jumlah) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= jumlah;
+                return true;
+            }
+        }
+
+        public bool ambilDollar(int jumlah)
+        {
+            int administrasi = jumlah * 50;
+            int konversiRupiah = jumlah * 15000;
+            if((saldo - (konversiRupiah+administrasi)) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - (konversiRupiah + administrasi);
+                return true;
+            }
+        }
+    }
+}
